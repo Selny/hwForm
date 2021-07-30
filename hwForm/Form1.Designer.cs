48e1e30b130753c9306755b75ae625751baff113
valiz <valiz@DESKTOP-ED381US>
2021-07-30 17:23:16 +0400

    Add project files.

---
+++ hwForm/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿
+namespace hwForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_surname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_gender_male = new System.Windows.Forms.RadioButton();
+            this.btn_gender_female = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cb_eng = new System.Windows.Forms.CheckBox();
+            this.cb_spa = new System.Windows.Forms.CheckBox();
+            this.cb_korean = new System.Windows.Forms.CheckBox();
+            this.cb_japanese = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.tb_mail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_address = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(152, 92);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(240, 31);
+            this.tb_name.TabIndex = 1;
+            // 
+            // tb_surname
+            // 
+            this.tb_surname.Location = new System.Drawing.Point(152, 174);
+            this.tb_surname.Name = "tb_surname";
+            this.tb_surname.Size = new System.Drawing.Size(240, 31);
+            this.tb_surname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(146, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(146, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date of birth:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(151, 250);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(402, 31);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(146, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gender:";
+            // 
+            // btn_gender_male
+            // 
+            this.btn_gender_male.AutoSize = true;
+            this.btn_gender_male.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_gender_male.Location = new System.Drawing.Point(151, 330);
+            this.btn_gender_male.Name = "btn_gender_male";
+            this.btn_gender_male.Size = new System.Drawing.Size(93, 29);
+            this.btn_gender_male.TabIndex = 7;
+            this.btn_gender_male.TabStop = true;
+            this.btn_gender_male.Text = "Male";
+            this.btn_gender_male.UseVisualStyleBackColor = true;
+            // 
+            // btn_gender_female
+            // 
+            this.btn_gender_female.AutoSize = true;
+            this.btn_gender_female.Font = new System.Drawing.Font("Trebuchet MS", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_gender_female.Location = new System.Drawing.Point(152, 365);
+            this.btn_gender_female.Name = "btn_gender_female";
+            this.btn_gender_female.Size = new System.Drawing.Size(109, 31);
+            this.btn_gender_female.TabIndex = 8;
+            this.btn_gender_female.TabStop = true;
+            this.btn_gender_female.Text = "Female";
+            this.btn_gender_female.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(147, 515);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Foreign Languages:";
+            // 
+            // cb_eng
+            // 
+            this.cb_eng.AutoSize = true;
+            this.cb_eng.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_eng.Location = new System.Drawing.Point(152, 561);
+            this.cb_eng.Name = "cb_eng";
+            this.cb_eng.Size = new System.Drawing.Size(119, 29);
+            this.cb_eng.TabIndex = 10;
+            this.cb_eng.Text = "English";
+            this.cb_eng.UseVisualStyleBackColor = true;
+            // 
+            // cb_spa
+            // 
+            this.cb_spa.AutoSize = true;
+            this.cb_spa.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_spa.Location = new System.Drawing.Point(151, 596);
+            this.cb_spa.Name = "cb_spa";
+            this.cb_spa.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cb_spa.Size = new System.Drawing.Size(127, 29);
+            this.cb_spa.TabIndex = 11;
+            this.cb_spa.Text = "Spanish";
+            this.cb_spa.UseVisualStyleBackColor = true;
+            // 
+            // cb_korean
+            // 
+            this.cb_korean.AutoSize = true;
+            this.cb_korean.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_korean.Location = new System.Drawing.Point(152, 631);
+            this.cb_korean.Name = "cb_korean";
+            this.cb_korean.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cb_korean.Size = new System.Drawing.Size(119, 29);
+            this.cb_korean.TabIndex = 12;
+            this.cb_korean.Text = "Korean";
+            this.cb_korean.UseVisualStyleBackColor = true;
+            // 
+            // cb_japanese
+            // 
+            this.cb_japanese.AutoSize = true;
+            this.cb_japanese.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_japanese.Location = new System.Drawing.Point(151, 666);
+            this.cb_japanese.Name = "cb_japanese";
+            this.cb_japanese.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cb_japanese.Size = new System.Drawing.Size(143, 29);
+            this.cb_japanese.TabIndex = 13;
+            this.cb_japanese.Text = "Japanese";
+            this.cb_japanese.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(508, 515);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(320, 29);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Programming Languages:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(513, 561);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 29);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "C";
+            this.checkBox1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(513, 596);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(93, 29);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "C++";
+            this.checkBox2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(513, 631);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(76, 29);
+            this.checkBox3.TabIndex = 17;
+            this.checkBox3.Text = "C#";
+            this.checkBox3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Verdana", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.Location = new System.Drawing.Point(513, 666);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(92, 29);
+            this.checkBox4.TabIndex = 18;
+            this.checkBox4.Text = "Java";
+            this.checkBox4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // tb_mail
+            // 
+            this.tb_mail.Location = new System.Drawing.Point(466, 92);
+            this.tb_mail.Name = "tb_mail";
+            this.tb_mail.Size = new System.Drawing.Size(240, 31);
+            this.tb_mail.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(460, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 29);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Mail:";
+            // 
+            // tb_address
+            // 
+            this.tb_address.Location = new System.Drawing.Point(466, 174);
+            this.tb_address.Name = "tb_address";
+            this.tb_address.Size = new System.Drawing.Size(240, 31);
+            this.tb_address.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(460, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(224, 29);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Workgin address:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(372, 779);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 52);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Submit!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(232)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(907, 900);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_address);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tb_mail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cb_japanese);
+            this.Controls.Add(this.cb_korean);
+            this.Controls.Add(this.cb_spa);
+            this.Controls.Add(this.cb_eng);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_gender_female);
+            this.Controls.Add(this.btn_gender_male);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_surname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_surname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton btn_gender_male;
+        private System.Windows.Forms.RadioButton btn_gender_female;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox cb_eng;
+        private System.Windows.Forms.CheckBox cb_spa;
+        private System.Windows.Forms.CheckBox cb_korean;
+        private System.Windows.Forms.CheckBox cb_japanese;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox tb_mail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_address;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ hwForm/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hwForm
+{
+    public partial class Form1 : Form
+    {
+        public class Info
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public bool Gender { get; set; }
+            public string Mail { get; set; }
+            public string WorkingAddress { get; set; }
+            public string ForeignLanguages { get; set; }
+            public string ProgrammingLanguages { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var info = new Info
+            {
+                Name = tb_name.Text,
+                Surname = tb_surname.Text,
+                DateOfBirth = dateTimePicker1.Value,
+                Gender = btn_gender_male.Checked,
+                Mail = tb_mail.Text,
+                WorkingAddress = tb_address.Text,
+            };
+            string jsonString = JsonSerializer.Serialize(info);
+            MessageBox.Show(jsonString);
+        }
+    }
+}
